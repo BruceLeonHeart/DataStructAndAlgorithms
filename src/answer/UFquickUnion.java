9b462aebf8841f1f5f011f610f5440f3e66199b6
Pz1_ad_04 <815341194@qq.com>
2019-05-13 16:53:15 +0800

    05-13

--- src/answer/UFquickUnion.java
+++ src/answer/UFquickUnion.java
@@ -3,12 +3,11 @@ package answer;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-public class UF {
-    private int[] id;  //分量id
-    private int count; //分量数目
+public class UFquickUnion {
+    private int[] id; // 分量id
+    private int count; // 分量数目
 
-
-    public UF(int N) {
+    public UFquickUnion(int N) {
         this.count = N;
         id = new int[N];
         for (int i = 0; i < N; i++) {
@@ -16,34 +15,35 @@ public class UF {
         }
     }
 
-    public int count(){
+    public int count() {
         return count;
     }
-    public boolean isConnected(int p ,int q){
+
+    public boolean isConnected(int p, int q) {
         return find(p) == find(q);
     }
 
-    public int find(int m){
-        return id[m];
+    public int find(int m) {
+        while(m!=id[m])
+            m = id[m];
+            return m;
     }
 
-    public void union(int p,int q){
-        int pId = id[p];
-        int qId = id[q];
-        if (pId==qId)
-        return;
-        //用后者节点所在的分量id覆盖之前的
-        for (int i = 0; i < id.length; i++) {
-            if (id[i] == pId)
-                id[i] = qId;            
-        }
-        count --;
+    public void union(int p, int q) {
+        int pId = find(p);
+        int qId = find(q);
+        if (pId == qId)
+            return;
+        // 用后者节点所在的分量id覆盖之前的
+        id[pId] = qId;
+        
+        count--;
 
     }
 
     public static void main(String[] args) {
         int N = StdIn.readInt();
-        UF uf = new UF(N);
+        UFquickUnion uf = new UFquickUnion(N);
         while(!StdIn.isEmpty()){
             int p = StdIn.readInt();
             int q = StdIn.readInt();
